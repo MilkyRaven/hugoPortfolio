6958883e88fa6d499d33f7fbe602bf3d60746ad1
MilkyRaven <milkykiwidev@gmail.com>
2023-03-28 13:43:29 +0200

    fixed lint error

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 export default function About() {
     return <div>
-        <Link href={'/'}>Return Home</Link>
+        <Link href={"/"}>Return Home</Link>
         <h1>About me</h1>
         <div className="pic"></div>
-        <h1>I'm Hugo super duper cool man</h1>
+        <h1>Im Hugo super duper cool man</h1>
         <p>Im hugo super duper cool man text is coool bro 
             little more text
             and we golden.
