df082fa47c2fb3cc0de17dd31646ed10c8a3e869
MilkyRaven <milkykiwidev@gmail.com>
2023-04-05 16:01:43 +0200

    working on data projects page

--- src/pages/data/index.tsx
+++ src/pages/data/index.tsx
@@ -8,7 +8,7 @@ export default function Data() {
         <div className="mt-14 ml-6 mr-6 mb-24">
             {/* <Link href={'/'}>Return Home</Link> */}
             <h1 className='text-4xl'>DATA</h1>
-            <Link href={'/data/project'}>
+            <Link href={'/data/project1'}>
                 <div className="flex mt-8 mb-8 place-content-between">
                     <div>
                         <h2 className="text-4xl">Project 1</h2>
@@ -17,7 +17,7 @@ export default function Data() {
                     <div className="w-16 h-36 bg-slate-300"></div>
                 </div>
             </Link>
-            <Link href={'/data/project'}>
+            <Link href={'/data/project2'}>
                 <div className="flex mt-8 mb-8 place-content-between">
                     <div>
                         <h2 className="text-4xl">Project 2</h2>
@@ -26,7 +26,7 @@ export default function Data() {
                     <div className="w-40 h-36 bg-slate-300"></div>
                 </div>
             </Link>
-            <Link href={'/data/project'}>
+            <Link href={'/data/project3'}>
                 <div className="flex mt-8 mb-8 place-content-between">
                     <div>
                         <h2 className="text-4xl">Project 3</h2>
--- src/pages/data/project.tsx
+++ src/pages/data/project.tsx
@@ -8,39 +8,41 @@ export default function project() {
             <Nav></Nav>
             <div className='p-8'>
                 <h2 className='text-4xl'>Data Project 1</h2>
-                <h3 className='text-3xl'>Analyzing Customer Behavior to Improve Sales in an Online Retail Store</h3>
-                <div className='w-20 h-20 bg-slate-300'></div>
-                <h4>Introduction</h4>
-                <p>Introduction:
-                    In this case study, we will explore how data analysis can be used to improve sales in an online retail store. Our client is an online store that sells a wide range of products. They have a website that receives a high volume of traffic, but they are not converting as many visitors into customers as they would like. The goal of this project is to identify patterns in customer behavior and make recommendations for how the store can improve its sales.</p>
-                <div className='w-20 h-20 bg-slate-300'></div>
-                <div>
+                <h3 className='text-3xl mt-10'>Analyzing Customer Behavior to Improve Sales in an Online Retail Store</h3>
+                <div className='w-80 h-40 mt-4 mb-4 bg-slate-300'></div>
+                <h4 className='font-bold'>Introduction</h4>
+                <p>In this case study, we will explore how data analysis can be used to improve sales in an online retail store. Our client is an online store that sells a wide range of products. They have a website that receives a high volume of traffic, but they are not converting as many visitors into customers as they would like. The goal of this project is to identify patterns in customer behavior and make recommendations for how the store can improve its sales.</p>
+                <div className='w-80 h-40 mt-4 mb-4 bg-slate-300'></div>
+                <div className='mt-8 mb-8'>
                     <p>Data Collection:</p>
                     <p>To start our analysis, we collected data from the stores website using Google Analytics. We tracked a variety of metrics, including pageviews, bounce rate, time on site, and conversion rate. We also collected demographic information about the stores visitors, including age, gender, and location.</p>
                 </div>
-                <div className='flex'>
-                    <p>Data Cleaning: Before we could begin analyzing the data, we needed to clean it. We removed any duplicate entries, fixed any formatting errors, and ensured that all the data was in the correct format for analysis.</p>
+                <div className='flex place-content-between'>
+                    <div className='w-44'>
+                        <p>Data Cleaning: Before we could begin analyzing the data, we needed to clean it. We removed any duplicate entries, fixed any formatting errors, and ensured that all the data was in the correct format for analysis.</p>
+                    </div>
+                    <div className='w-28 h-72 bg-slate-300'></div>
                 </div>
-                <div>
+                <div className='mt-8 mb-8'>
                     <p>Data Analysis:</p>
                     <p>We used a variety of analytical techniques to explore the data and identify patterns. Some of the techniques we used include:
                         Segmentation analysis - We segmented the stores customers based on demographic data to identify patterns in customer behavior. We looked at age, gender, and location to see if there were any correlations between these factors and customer behavior.
                         Funnel analysis - We looked at the conversion funnel to identify where customers were dropping off in the purchasing process. We looked at each step of the funnel, from landing page to checkout, to see if there were any bottlenecks.
                         Cohort analysis - We analyzed customer behavior over time to identify any trends or patterns. We looked at how customer behavior changed over time, and whether there were any seasonal trends.</p>
-                    <div className='w-20 h-20 bg-slate-300'></div>
+                    <div className='w-80 h-40 mt-4 mb-4 bg-slate-300'></div>
                 </div>
                 <div>
                     <p>Data Visualization:</p>
                     <p>To communicate our findings, we used a variety of data visualizations. We used charts and graphs to display patterns in customer behavior, and heat maps to show where customers were spending the most time on the website. We also used dashboards to display key metrics, such as conversion rate and revenue, in real time.</p>
-                    <div className='flex place-content-between'>
-                    <div className='w-20 h-20 bg-slate-300'></div>
-                    <div className='w-20 h-20 bg-slate-300'></div>
-                    <div className='w-20 h-20 bg-slate-300'></div>
+                    <div className='flex place-content-evenly mt-8 mb-8'>
+                        <div className='w-20 h-40 bg-slate-300'></div>
+                        <div className='w-20 h-40 bg-slate-300'></div>
+                        <div className='w-20 h-40 bg-slate-300'></div>
                     </div>
                 </div>
                 <div className='flex justify-end place-items-center mt-4'>
                     <p>Project 2</p>
-                    <div className='w-10 h-10 bg-slate-300'></div>
+                    <div className='w-10 h-10 bg-slate-300 ml-4'></div>
                 </div>
             </div>
             <Footer></Footer>
