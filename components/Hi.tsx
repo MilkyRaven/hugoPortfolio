a4c1df784ca11f95f29fa2f2a2ea86ad6f260255
Milky Raven <heartonllamas@gmail.com>
2023-04-08 00:55:38 +0200

    adding main page pics

--- components/Hi.tsx
+++ components/Hi.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 
 type Props = {}
 
 const Hi = (props: Props) => {
     return (
-        <div>
-            <div className='w-full h-64 bg-slate-300'>
-                {/* <FaBars /> */}
-                <h1 className='text-4xl p-4'>Hi, welcome to my portfolio</h1>
+        <div className="relative w-full h-64">
+            <div className="absolute top-0 left-0 w-full h-full bg-slate-300 opacity-80"></div>
+            <div className="absolute top-0 left-0 w-full h-full flex items-end justify-center">
+                <h1 className="text-4xl text-white p-4 z-10">
+                    Hi, welcome to my portfolio
+                </h1>
+                <Image
+                    alt="hugo"
+                    src="/pics/hugo.jpeg"
+                    className="object-cover w-full h-full"
+                    layout='fill'
+                    objectFit='cover'
+                />
             </div>
         </div>
     )
