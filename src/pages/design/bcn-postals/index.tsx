7b176e95cba566cffbc6c5a3d123cc346e3a7bbd
Milky Raven <heartonllamas@gmail.com>
2023-04-16 18:15:52 +0200

    bcn postals section completed

--- src/pages/design/bcn-postals/index.tsx
+++ src/pages/design/bcn-postals/index.tsx
@@ -6,24 +6,41 @@ export default function project() {
     return (
         <div>
             <Nav></Nav>
-            <div className='p-8'>
-                <h2 className='text-4xl'>Design Project 1</h2>
-                <h3 className='text-3xl mt-10'>Rebranding for a Local Coffee Shop</h3>
-                <div className='w-80 h-40 mt-4 mb-4 bg-slate-300'></div>
-                <h4 className='font-bold'>Introduction</h4>
-                <p>Introduction:
-                    The local coffee shop, Java Junction, has been in business for over 10 years. While they have a loyal customer base, they have been struggling to attract new customers and keep up with the competition. They approached me to rebrand their coffee shop to attract a younger, more hip audience.</p>
-                <div className='w-80 h-40 mt-4 mb-4 bg-slate-300'></div>
-                <div className='flex place-content-evenly mt-8 mb-8'>
-                    <div className='w-20 h-40 bg-slate-300'></div>
-                    <div className='w-20 h-40 bg-slate-300'></div>
-                    <div className='w-20 h-40 bg-slate-300'></div>
+            <div>
+                <h2 className='text-4xl ml-8'>BCN POSTALS</h2>
+                <h3 className='text-3xl mt-4 ml-8'>so much more than a postcard
+                </h3>
+                <div className='flex flex-col items-center'>
+                    <div className='w-[361px] h-[606px] mt-4 mb-4 bg-slate-300'></div>
                 </div>
-                <div className='w-80 h-40 mt-4 mb-4 bg-slate-300'></div>
-                <div className='flex justify-center'>
-                    <div className='w-52 h-40 mt-4 mb-4 bg-slate-300'></div>
+                <p className='ml-10 mr-10 mt-9 mb-9'>“BCN POSTALS” is a series of fun, minimal postcards which represent 5 key elements of Barcelona’s identity. Art. Arquitectura. Barri. Gastronmia. And of course la Platija. </p>
+                <div className='flex flex-col items-center'>
+                    <div className='w-[280px] h-[454px] mt-4 mb-4 bg-slate-300'></div>
+                    <p>Postal package model.</p>
                 </div>
+                <div className='flex flex-col items-center'>
+                    <div className='w-[280px] h-[454px] mt-4 mb-4 bg-slate-300'></div>
+                    <p>Postal envelope model.</p>
+                </div>
+                <p className='ml-10 mr-10 mt-9 mb-9'>A postcard isn't just about what you see, it's also about how it makes you feel. BCN Postals achieves that through a more interactive and stimulating experience.</p>
+                {/* <div className='flex flex-row'>
+                    <div className='w-[177px] h-[326px]'></div>
+                    <div className='w- h-'></div>
+                </div> */}
+                <div className='flex flex-col items-center'>
+                    <div className='w-[363px] h-[210px] bg-slate-300'>
+                    </div>
+                    <p className='ml-4'>Painted acrylic postcards, 'liquid' casing & custom envelopes.</p>
+                </div>
+                <p className='text-xl m-10'>Closing statement for extra coolness
+                    postals son vida, vida son postals idk your the artist think of something.</p>
             </div>
+            <Link href={'/design/reimagining-urban-spaces'}><div className='flex justify-end'>
+                <div className='flex shadow-xl w-[164px] h-[77px] p-2 rounded-xl items-center'>
+                    <p>Reimagining Urban Spaces</p>
+                    <div className='w-10 h-10 p-4 bg-slate-300'></div>
+                </div>
+            </div></Link>
             <Footer></Footer>
         </div>
     )
