e9c3bb026ddeb7684a779ba554a7de50d908dc0a
Milky Raven <heartonllamas@gmail.com>
2023-04-16 19:11:59 +0200

    added data project

--- src/pages/data/eu-ukranian-refugees.tsx
+++ src/pages/data/eu-ukranian-refugees.tsx
@@ -6,43 +6,72 @@ export default function project() {
     return (
         <div>
             <Nav></Nav>
-            <div className='p-8'>
-                <h2 className='text-4xl'>Data Project 1</h2>
-                <h3 className='text-3xl mt-10'>Analyzing Customer Behavior to Improve Sales in an Online Retail Store</h3>
-                <div className='w-80 h-40 mt-4 mb-4 bg-slate-300'></div>
-                <h4 className='font-bold'>Introduction</h4>
-                <p>In this case study, we will explore how data analysis can be used to improve sales in an online retail store. Our client is an online store that sells a wide range of products. They have a website that receives a high volume of traffic, but they are not converting as many visitors into customers as they would like. The goal of this project is to identify patterns in customer behavior and make recommendations for how the store can improve its sales.</p>
-                <div className='w-80 h-40 mt-4 mb-4 bg-slate-300'></div>
-                <div className='mt-8 mb-8'>
-                    <p>Data Collection:</p>
-                    <p>To start our analysis, we collected data from the stores website using Google Analytics. We tracked a variety of metrics, including pageviews, bounce rate, time on site, and conversion rate. We also collected demographic information about the stores visitors, including age, gender, and location.</p>
+            <div>
+                <h2 className='text-4xl ml-8'>THE EU & UKRANIAN REFUGEES</h2>
+                <div className='flex flex-col items-center'>
+                    <div className='w-[373px] h-[228px] mt-4 mb-4 bg-slate-300'></div>
                 </div>
-                <div className='flex place-content-between'>
-                    <div className='w-44'>
-                        <p>Data Cleaning: Before we could begin analyzing the data, we needed to clean it. We removed any duplicate entries, fixed any formatting errors, and ensured that all the data was in the correct format for analysis.</p>
-                    </div>
-                    <div className='w-28 h-72 bg-slate-300'></div>
-                </div>
-                <div className='mt-8 mb-8'>
-                    <p>Data Analysis:</p>
-                    <p>We used a variety of analytical techniques to explore the data and identify patterns. Some of the techniques we used include:
-                        Segmentation analysis - We segmented the stores customers based on demographic data to identify patterns in customer behavior. We looked at age, gender, and location to see if there were any correlations between these factors and customer behavior.
-                        Funnel analysis - We looked at the conversion funnel to identify where customers were dropping off in the purchasing process. We looked at each step of the funnel, from landing page to checkout, to see if there were any bottlenecks.
-                        Cohort analysis - We analyzed customer behavior over time to identify any trends or patterns. We looked at how customer behavior changed over time, and whether there were any seasonal trends.</p>
-                    <div className='w-80 h-40 mt-4 mb-4 bg-slate-300'></div>
-                </div>
-                <div>
-                    <p>Data Visualization:</p>
-                    <p>To communicate our findings, we used a variety of data visualizations. We used charts and graphs to display patterns in customer behavior, and heat maps to show where customers were spending the most time on the website. We also used dashboards to display key metrics, such as conversion rate and revenue, in real time.</p>
-                    <div className='flex place-content-evenly mt-8 mb-8'>
-                        <div className='w-20 h-40 bg-slate-300'></div>
-                        <div className='w-20 h-40 bg-slate-300'></div>
-                        <div className='w-20 h-40 bg-slate-300'></div>
+                < div className='ml-10 mr-10 mt-9 mb-9'>
+                    <div>
+                        <p className='text-3xl'> Introduction</p>
+                        <p className='text-xl'>Project Focus</p>
+                        <p className='text-xl'>To invesitgate whether there is a relationship between EU memberstate's economic strength (understood in terms of GDP) and the amount of Ukrainian refugees hosted by these countries.</p>
                     </div>
                 </div>
-                <div className='flex justify-end place-items-center mt-4'>
-                    <p>Project 2</p>
-                    <div className='w-10 h-10 bg-slate-300 ml-4'></div>
+                < div className='ml-10 mr-10 mt-9 mb-9'>
+                    <div>
+                        <p className='text-xl'>Questions & Hypothesis</p>
+                        <p className='text-xl'>
+                            Do countries with a higher GDP accept larger quantites of refugees?
+                            Is there a correlation between refugee numbers and distance to Ukraine?
+                            Can larger EU economies accept more refugees?
+                            Hypothesis: Countries with a closer proximity to Ukraine have a higher intake of refugees. However that have displayed an 'open boarder' policy in recent to refugees would also accept higher numbers. I believe there to be a moral case for EU member states with a high GDP rate to accept more refugees.</p>
+                    </div>
+                </div>
+                <div className='ml-10 mr-10 mt-9 mb-9'>
+                    <div>
+                        <p className='text-xl'>Tools & Method</p>
+                        <p className='text-xl'>
+                            For this particular project we had to demonstrate usage of APIs, webscraping or both. I scraped three websites for the data used for this project.</p>
+                    </div>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <div className='w-[384px] h-[192px] mt-4 mb-4 bg-slate-300'></div>
+                </div>
+                < div className='ml-10 mr-10 mt-9 mb-9'>
+                    <div>
+                        <p className='text-xl'>EU member state economic infomation: https://countryeconomy.com/countries/
+                            groups/european-union
+                            Refugee numbers: https://www.statista.com/statistics
+                            /1312361/europe-temporary-protection-for-persons-fleeing-ukraine/
+                            Distance from Ukraine: https://www.distancefromto.net/distance-from-ukraine-country</p>
+                        <p className='text-xl m-'>
+                            I imported the following tools to run the code:
+
+                            import matplotlib.pyplot as plt
+                            from matplotlib import rcParams
+                            import requests
+                            from bs4 import BeautifulSoup
+                            import pandas as pd
+                            import requests
+                            import re
+                            import seaborn as sns
+                            import matplotlib.pyplot as plt
+                            import numpy as np
+                            from matplotlib import rcParams</p>
+                    </div>
+                    <div className='flex flex-col items-center'>
+                        <div className='w-[384px] h-[192px] mt-4 mb-4 bg-slate-300'></div>
+                    </div>
+                    <div className='flex flex-col items-center'>
+                        <div className='w-[384px] h-[192px] mt-4 mb-4 bg-slate-300'></div>
+                    </div>
+                    <div className='flex flex-col items-center'>
+                        <div className='w-[384px] h-[192px] mt-4 mb-4 bg-slate-300'></div>
+                    </div>
+                    <div className='flex flex-col items-center'>
+                        <div className='w-[384px] h-[192px] mt-4 mb-4 bg-slate-300'></div>
+                    </div>
                 </div>
             </div>
             <Footer></Footer>
