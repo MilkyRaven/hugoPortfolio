d83733494772b213479a10df7e8d0e3aa90cbf7b
Milky Raven <heartonllamas@gmail.com>
2023-04-16 13:47:37 +0200

    finished photography index section

--- src/pages/photography/index.tsx
+++ src/pages/photography/index.tsx
@@ -10,8 +10,8 @@ export default function Photography() {
             <p className="text-xl mt-2">This is a text about what I do with my photography work, how I got here, why, and anything else to give readers a feel for what they might encounter in this section</p>
         </div>
 
-        <Link href={'/photography/gallery1'}><div className="bg-slate-300 w-80 h-[420px] m-10">
-            <div className="bg-black text-white p-4 opacity-70">
+        <Link href={'/photography/gallery1'}><div className="bg-slate-300 w-80 h-[420px] m-10 relative">
+            <div className="bg-black text-white p-4 opacity-70 absolute bottom-0 w-full">
                 <p className="text-4xl">Title of the Project</p>
                 <p className="text-base mt-2">Description of the project</p>
             </div>
