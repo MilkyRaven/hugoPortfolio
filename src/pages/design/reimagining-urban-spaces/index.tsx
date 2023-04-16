07c3deb77c8d59fd35b3481d9a8de36858c9d6bc
Milky Raven <heartonllamas@gmail.com>
2023-04-16 18:43:57 +0200

    urban spaces project done

--- src/pages/design/reimagining-urban-spaces/index.tsx
+++ src/pages/design/reimagining-urban-spaces/index.tsx
@@ -7,24 +7,53 @@ export default function project() {
     return (
         <div>
             <Nav></Nav>
-            <div className='p-8'>
-                <h2 className='text-4xl'>Design Project 1</h2>
-                <h3 className='text-3xl mt-10'>Rebranding for a Local Coffee Shop</h3>
-                <div className='w-80 h-40 mt-4 mb-4 bg-slate-300'></div>
-                <h4 className='font-bold'>Introduction</h4>
-                <p>Introduction:
-                    The local coffee shop, Java Junction, has been in business for over 10 years. While they have a loyal customer base, they have been struggling to attract new customers and keep up with the competition. They approached me to rebrand their coffee shop to attract a younger, more hip audience.</p>
-                <div className='w-80 h-40 mt-4 mb-4 bg-slate-300'></div>
-                <div className='flex place-content-evenly mt-8 mb-8'>
-                    <div className='w-20 h-40 bg-slate-300'></div>
-                    <div className='w-20 h-40 bg-slate-300'></div>
-                    <div className='w-20 h-40 bg-slate-300'></div>
+            <div>
+                <h2 className='text-4xl ml-8'>REIMAGINING URBAN SPACES</h2>
+                <h3 className='text-3xl mt-4 ml-8'>Challenging the orthodoxy
+                </h3>
+                <div className='flex flex-col items-center'>
+                    <div className='w-[361px] h-[606px] mt-4 mb-4 bg-slate-300'></div>
                 </div>
-                <div className='w-80 h-40 mt-4 mb-4 bg-slate-300'></div>
-                <div className='flex justify-center'>
-                    <div className='w-52 h-40 mt-4 mb-4 bg-slate-300'></div>
+                <p className='ml-10 mr-10 mt-9 mb-9'>The idea behind this collection was to create a series of posters which sought to challenge our conception of urban spatial orthodoxy.</p>
+                <div className='flex flex-col items-center'>
+                    <div className='w-[272px] h-[403px] mt-4 mb-4 bg-slate-300'></div>
+                    <p>Posters in abandoned space.</p>
                 </div>
+                <p className='ml-10 mr-10 mt-9 mb-9'>Where I live in Gracia, there are no green spaces. Just a couple of trees and concrete plazas. </p>
+                {/* Add carousel */}
+                <p className='ml-10 mr-10 mt-9 mb-9'>Using photos of the Catalan countryside I wanted to design the posters with a focus of mimicking the sense of a window and looking into an alternative version of the given urban space. Together with a tag line and prompt below to guide the users.</p>
+                {/* Add carousel */}
+                <div className='flex flex-col items-center'>
+                    <div className='flex flex-row'>
+                        <div className='w-[170px] h-[241px] m-1 bg-slate-300'></div>
+                        <div className='w-[170px] h-[241px] m-1  bg-slate-300'></div>
+                    </div>
+                    <p className='m-2'>Posters: interactive questionnaire.</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <div className='w-[272px] h-[403px] mt-4 mb-4 bg-slate-300'></div>
+                    <p>Posters and interactive questionnaire</p>
+                </div>
+                <div className='flex flex-col items-center mt-12 '>
+                    <div className='flex flex-row'>
+                        <div className=' m-1 bg-slate-300 w-[158px] h-[224px]'></div>
+                        <div className=' m-1 bg-slate-300 w-[158px] h-[224px]'></div>
+                    </div>
+                    <div className='flex flex-row'>
+                        <div className='m-1 bg-slate-300 w-[158px] h-[224px]'></div>
+                        <div className='m-1 bg-slate-300 w-[158px] h-[224px]'></div>
+                    </div>
+                    <p>Booklet: reimagining urban spaces.</p>
+                </div>
+                <p className='text-xl m-10'>Closing statement for extra coolness
+                    postals son vida, vida son postals idk your the artist think of something.</p>
             </div>
+            <Link href={'/design/bcn-postals'}><div className='flex justify-start'>
+                <div className='flex shadow-xl w-[164px] h-[77px] p-2 rounded-xl items-center'>
+                    <div className='w-10 h-10 p-4 m-2 bg-slate-300'></div>
+                    <p>BCN postals</p>
+                </div>
+            </div></Link>
             <Footer></Footer>
         </div>
     )
