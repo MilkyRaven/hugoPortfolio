c7daf634bfb290bee0b319c56b666d7d08afe7ad
Milky Raven <heartonllamas@gmail.com>
2023-04-21 21:41:17 +0200

    working on contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,8 +5,14 @@ import Footer from "../../components/Footer"
 export default function Contact() {
     return <div>
         <Nav></Nav>
-        <h1 className="text-4xl ml-10">Contact</h1>
-        <div className="bg-slate-300 w-auto h-32 mt-5 mb-5"></div>
+        <h1 className="text-[40px] mt-[10px] ml-10 font-crimson">GET IN TOUCH</h1>
+        <div className="static text-white">
+            <div className="bg-slate-400 w-auto h-[254px] mt-2 mb-5 static flex-col justify-stretch">
+                <p className="font-thin text-[32px] pl-10 pr-10 pt-5">Have a project you would like to work on together?</p>
+                <p className="text-[20px] pl-10 pt-10">I wanna hear about it!</p>
+            </div>
+        </div>
+
         <div className="flex flex-col items-start ml-4">
             <Link href={'mailto:hugosalaimo@gmail.com'}><button className=" bg-super-light-blue text-white mb-2 rounded-lg shadow-lg"><p className="text-4xl p-3">E-mail</p></button></Link>
             <Link target="_blank" href={'https://www.linkedin.com/in/hugoalaimo/'}><button className=" bg-light-blue text-white mb-2 rounded-lg shadow-lg"><p className="text-4xl p-3">LinkedIn</p></button></Link>
