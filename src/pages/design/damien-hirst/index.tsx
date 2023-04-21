e8bd3b8150cead79378060f846e285ad41821da7
Milky Raven <heartonllamas@gmail.com>
2023-04-21 21:14:06 +0200

    changed project button and made it component

--- src/pages/design/damien-hirst/index.tsx
+++ src/pages/design/damien-hirst/index.tsx
@@ -15,12 +15,12 @@ export default function project() {
                 <div className='flex flex-col items-center'>
                     <div className='w-[376px] h-[536px] mt-4 mb-4 bg-slate-300'></div>
                 </div>
-                <p className='ml-10 mr-10 mt-9 mb-9'>For this project I was to design the visual identity for artist Damian Hirst's upcoming exhibition. I researched Hirst's previous work and the target audience to create a cohesive design language that would work across mediums like logo, posters and banners.</p>
+                <p className='ml-10 mr-10 mt-9 mb-9'>For this project I was to design the visual identity for artist Damien Hirst&apos;s upcoming exhibition. I researched Hirst&apos;s previous work and the target audience to create a cohesive design language that would work across mediums like logo, posters and banners.</p>
                 <div className='flex flex-col items-center'>
                     <div className='w-[281px] h-[398px] mt-4 mb-4 bg-slate-300'></div>
                     <p>Poster 2</p>
                 </div>
-                <p className='ml-10 mr-10 mt-9 mb-9'>Using a bold color palette of pink green and yellow, exploring desaturated neon and striking black and white imagery, I developed a series of posters and banners that captured the essence of Hirst's art.</p>
+                <p className='ml-10 mr-10 mt-9 mb-9'>Using a bold color palette of pink green and yellow, exploring desaturated neon and striking black and white imagery, I developed a series of posters and banners that captured the essence of Hirst&apos;s art.</p>
                 <div className='flex flex-col items-center'>
                     <div className='w-[355px] h-[238px] mt-4 mb-4 bg-slate-300'></div>
                     <p>Logo</p>
