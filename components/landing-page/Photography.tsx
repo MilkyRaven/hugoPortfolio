198b748ff7c32af4b5e80a20a22b230fa16591bd
Milky Raven <heartonllamas@gmail.com>
2023-04-21 19:53:49 +0200

    adding fonts

--- components/landing-page/Photography.tsx
+++ components/landing-page/Photography.tsx
@@ -9,7 +9,7 @@ export default function Photography(props: Props) {
             <Link href={'/photography'}>
                 <div className="flex-col h-[354px] pl-6 mt-8 ml-8 mb-8 pb-6 place-content-between rounded-md shadow-md">
                     <div className='mr-16'>
-                        <h2 className="text-4xl pt-4">PHOTOGRAPHY</h2>
+                        <h2 className="text-4xl pt-4 font-light">PHOTOGRAPHY</h2>
                         <h3 className="text-2xl pb-4">Here you will find a sample of my work in photography</h3>
                     </div>
                     <div className="mt-5 h-36 w-fill bg-slate-300"></div>
